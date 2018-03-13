9e859884bc9b682d828d19df55e169c7a7251d85
oiuv <i@oiuv.cn>
2018-03-13 23:42:15 +0800

    required vendor

--- index.php
+++ index.php
@@ -4,8 +4,9 @@
 session_start();
 set_time_limit(0);
 $t1 = microtime();
-define("PE_VERSION",'4.2');
+define("PE_VERSION", '4.2');
 require "lib/init.cls.php";
+require "vendor/autoload.php";
 
 $ginkgo = new ginkgo;
 $ginkgo->run();
