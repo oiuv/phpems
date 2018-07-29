40d9297600d1430fa58e5406b92ef780bada3d9b
oiuv <i@oiuv.cn>
2018-07-29 07:06:24 +0000

    新增考试记录数据处理

--- model/ExamHistory.php
+++ model/ExamHistory.php
@@ -33,4 +33,28 @@ class ExamHistory extends Model
     {
         return $this->belongsTo(User::class, 'ehuserid');
     }
+
+    // 加密压缩考试试题
+    public function setEhquestionAttribute($value)
+    {
+        $this->attributes['ehquestion'] = base64_encode(gzcompress(serialize($value), 9));
+    }
+
+    // 加密压缩考试设置
+    public function setEhsettingAttribute($value)
+    {
+        $this->attributes['ehsetting'] = base64_encode(gzcompress(serialize($value), 9));
+    }
+
+    // 解密考试试题
+    public function getEhquestionAttribute($value)
+    {
+        return unserialize(gzuncompress(base64_decode($value)));
+    }
+
+    // 解密考试设置
+    public function getEhsettingAttribute($value)
+    {
+        return unserialize(gzuncompress(base64_decode($value)));
+    }
 }
