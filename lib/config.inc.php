45ed11e27307bc3d2e9be3a6b4cb58b8e312f0fa
oiuv <i@oiuv.cn>
2018-07-29 23:30:17 +0800

    开启微信登录

--- lib/config.inc.php
+++ lib/config.inc.php
@@ -9,7 +9,7 @@ define('CS',md5(base64_encode($_SERVER['HTTP_HOST'])));
 define('HE','utf-8');
 define('PN',10);
 define('TIME',time());
-define('USEWX',FALSE);
+define('USEWX',TRUE);
 if(dirname($_SERVER['SCRIPT_NAME']))
 define('WP','http://'.$_SERVER['SERVER_NAME'].dirname($_SERVER['SCRIPT_NAME']));
 else
@@ -17,17 +17,17 @@ define('WP','http://'.$_SERVER['SERVER_NAME'].'/');
 
 define('DB','phpems');//MYSQL数据库名
 define('DH','127.0.0.1');//MYSQL主机名，不用改
-define('DU','homestead');//MYSQL数据库用户名
-define('DP','secret');//MYSQL数据库用户密码
+define('DU','root');//MYSQL数据库用户名
+define('DP','');//MYSQL数据库用户密码
 define('DTH','x2_');//系统表前缀，不用改
 
 
-define('WXAPPID','wx0f2a632abf082e60');
-define('WXAPPSECRET','3368f73a6f517b37c7e8f0cee2c86341');
-define('WXMCHID','1369875702');
+define('WXAPPID','wxeb0ce70607ad267f');
+define('WXAPPSECRET','5b0b87722c268d89a82bcb40e3853f29');
+define('WXMCHID','7300637');
 define('WXKEY','zhelishi32weidewxkey');
 
 
-define('ALIPART','2088121450284825');
-define('ALIKEY','j8tnfkx7l0wddmxyfytzt0kdkuaitkiw');
-define('ALIACC','suoware@126.com');
+define('ALIPART','7300637');
+define('ALIKEY','5b0b87722c268d89a82bcb40e3853f29');
+define('ALIACC','i@oiuv.cn');
